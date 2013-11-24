51c1fc8e4ade455fe9c99a64fdf19f58ab6844ac
Sherry Shao <sshao0123@gmail.com>
2013-11-23 18:02:07 -0800

    Added comments

--- project2.pl
+++ project2.pl
@@ -4,13 +4,17 @@
 
 % start: Sets player character, number of players and player turn.
 start :- 
+	% set player's character
 	write('Who is your character?'), nl,
 	read(Player), 
+	% check for invalid characters
 	( suspect(Player) -> asserta(iam(Player)), nl
 	; nl, write('That was not a valid character.'), nl, start
 	),
+	% set number of players
 	write('How many players are there this round?'), nl,
 	read(Num), asserta(numPlayers(Num)), nl,
+	% set turn number for player
 	write('Which turn are you? example: 2'), nl,
 	read(Turn), asserta(turn(Turn)), nl,
 	write('It is my pleasure to be of your assistance! You are now set as '), write(Player), 
@@ -23,6 +27,7 @@ gethand :-
 	write('Please enter a card or type \'done\', example: mustard, ballroom, reolver, etc.'), nl,
 	read(Card),
 	( Card \= done -> 
+		% check for invalid cards
 		( possible(Card) -> retract(possible(Card)), nl, gethand
 		; nl, write('That was not a valid card.'), nl, gethand
 		)
@@ -33,32 +38,44 @@ gethand :-
 gameplay(Num) :-
 	nl,nl,
 	( turn(Num) ->
+		% Player's turn
 		write('It is our turn!'), nl,
 
-		% Suggest a suggestion / accusation here, for example
+		% Check if accusation is possible (a valid combination + only 3 possible objects left)
 		( possible(X),suspect(X),possible(Y),weapon(Y),possible(Z),room(Z),aggregate_all(count, possible(_), Count),Count is 3 ->
 		  write('Good news! I have solved the case, good sir! Please make the following accusation:'), nl,
 		  write('Suspect: '), write(X), write(', Weapon: '), write(Y), write(', Room: '), write(Z), nl, nl
+
+		% Check if suggestion is possible (a valid combination)
 		; possible(X),suspect(X),possible(Y),weapon(Y),possible(Z),room(Z) -> 
 		  write('Here is your next suggestion if you are available to give one: '), nl,
 		  write('Suspect: '), write(X), write(', Weapon: '), write(Y), write(', Room: '), write(Z), nl, nl
+
+		% Check.. if something went wrong and we've removed way too many clauses
 		; write('Well this is awkward, I cannot find any more possible combinations... I am afraid you are on your own.'), nl, nl
 		),
 
+		% Player choose to accuse, suggest or skip in their turn
 		write('Please enter \'a\' for accusation, \'s\' for suggestion or \'skip\' to skip your turn.'), nl, nl,
 		read(Input),
 		( Input \= skip ->
 			write('Please enter a suspect: '), nl, read(Suspect), nl,
 			write('Please enter a weapon: '), nl, read(Weapon), nl,
 			write('Please enter a room: '), nl, read(Room), nl,
+
+			% Player chose to make an accusation
 			( Input = a ->
 				write('Was the result of our deduction correct? (y/n)'), nl, nl, read(AccusationResult),
 				( AccusationResult = y -> win
 				; AccusationResult = n -> lose
 				)
+
+			% Player chose to make a suggestion
 			; Input = s ->
 				write('Please enter a card if you were shown one, otherwise, enter \'none\', example: mustard, ballroom, reolver, etc.'), nl,
 				read(SuggestionResult),
+
+				% If player was shown a card, remove it from the possible deck; loop
 				( SuggestionResult \= none ->
 					retract(possible(SuggestionResult)), nextNum(Num, NewNum), gameplay(NewNum)
 				; nextNum(Num, NewNum), gameplay(NewNum)
@@ -66,21 +83,31 @@ gameplay(Num) :-
 			)
 		; nextNum(Num, NewNum), gameplay(NewNum)
 		)
+
+	% Other players' turns
 	; write('It is Player '), write(Num), write('\'s turn'), nl,
 	  write('Has anyone figured it out yet? '), nl,
+
+	  % Player lets assistant know if other players have made accusations or suggestions
 	  write('Please enter \'a\' for accusation, \'s\' for suggestion or \'skip\' to skip their turn.'), nl,
 	  read(Input),
 		( Input \= skip ->
 			write('Please enter a suspect: '), read(Suspect), nl,
 			write('Please enter a room: '), read(Room), nl,
 			write('Please enter a weapon: '), read(Weapon), nl,
+
+			% If other players made an accusation (we lose if they were correct, else loop)
 			( Input = a ->
 				write('Was the result of their deduction correct? (y/n)'), nl, read(AccusationResult),
 				( AccusationResult = y -> lose
 				; AccusationResult = n -> nextNum(Num, NewNum), gameplay(NewNum)
 				)
+
+			% If other players made a suggestion
 			; Input = s ->
 				write('Was a card shown after their suggestion? (y/n)'), nl, read(SuggestionResult),
+
+				% Do something if a card is shown
 				( SuggestionResult = y -> nextNum(Num, NewNum), gameplay(NewNum)
 				; SuggestionResult = n -> nextNum(Num, NewNum), gameplay(NewNum)
 				)
@@ -103,6 +130,8 @@ win :- write('Congratulations, results show that we have conquered this majestic
 :- dynamic room/1.
 :- dynamic possible/1.
 
+% Suspects 
+
 suspect(plum).
 suspect(scarlet).
 suspect(mustard).
@@ -110,12 +139,16 @@ suspect(green).
 suspect(white).
 suspect(peacock).
 
+% Weapons
+
 weapon(knife).
 weapon(candlestick).
 weapon(revolver).
 weapon(rope).
 weapon(pipe).
 weapon(wrench).
+
+% Rooms
 
 room(kitchen).
 room(ballroom).
@@ -126,6 +159,8 @@ room(study).
 room(hall).
 room(lounge).
 room(dining).
+
+% Current Possible Objects
 
 possible(plum).
 possible(scarlet).
