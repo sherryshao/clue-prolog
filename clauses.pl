f41d3e0eee7af903074dd0a6e2bab49db4faa2bb
Sherry Shao <sshao0123@gmail.com>
2013-11-26 12:53:29 -0800

    Added testing file

---
+++ clauses.pl
@@ -0,0 +1,56 @@
+% Suspects 
+
+suspect(plum).
+suspect(scarlet).
+suspect(mustard).
+suspect(green).
+suspect(white).
+suspect(peacock).
+
+% Rooms
+
+room(kitchen).
+room(ballroom).
+room(conservatory).
+room(billiard).
+room(library).
+room(study).
+room(hall).
+room(lounge).
+room(dining).
+
+% Weapons
+
+weapon(knife).
+weapon(candlestick).
+weapon(revolver).
+weapon(rope).
+weapon(pipe).
+weapon(wrench).
+
+% Current Possible Objects
+
+possible(plum).
+possible(scarlet).
+possible(mustard).
+possible(green).
+possible(white).
+possible(peacock).
+
+possible(knife).
+possible(candlestick).
+possible(revolver).
+possible(rope).
+possible(pipe).
+possible(wrench).
+
+possible(kitchen).
+possible(ballroom).
+possible(conservatory).
+possible(billiard).
+possible(library).
+possible(study).
+possible(hall).
+possible(lounge).
+possible(dining).
+
